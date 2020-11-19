55f78e754cd8288fe3a86da92266f74319360818
Euan Ramsay <euan@Euans-MacBook-Pro.local>
2020-11-19 12:35:23 +0000

    Add beans and component.

--- src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
+++ src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -3,6 +3,8 @@ package com.udacity.jwdnd.c1.review;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 
@@ -12,4 +14,21 @@ public class ReviewApplication {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
 
+	@Bean
+	public String message() {
+		System.out.println("Creating message bean");
+		return "Hello, Spring";
+	}
+
+	@Bean
+	public String uppercaseMessage(MessageService messageService) {
+		System.out.println("Creating uppercaseMessage bean");
+		return messageService.uppercase();
+	}
+
+	@Bean
+	public String lowercaseMessage(MessageService messageService) {
+		System.out.println("Creating lowercaseMessage bean");
+		return messageService.lowercase();
+	}
 }
